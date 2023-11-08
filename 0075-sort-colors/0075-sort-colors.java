95f1bd8a36813782674e7946a85eb25f0ac2375f
MD-MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-11-08 11:23:08 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (17.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,39 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        
-        for(int i=0;i<nums.length;i++)
+        int low = 0;
+        int high = nums.length-1;
+        quickSort(nums,low,high);
+    }
+    
+    void quickSort(int[] arr, int low, int high)
+    {
+        if(low < high)
         {
-            for(int j=0;j<nums.length;j++)
-            {
-                if(nums[i] < nums[j])
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-            
-            
+            int pivot = partition(arr,low,high);
+            quickSort(arr,low,pivot);
+            quickSort(arr,pivot+1,high);
+        }
+    }
+    
+    int partition(int[] arr, int low, int high)
+    {
+     int pivot = arr[low];
+        int i=low-1,j=high+1;
+        
+        while(true)
+        {
+            do{
+                i++;
+            }while(arr[i] < pivot);
+                do{
+                    j--;
+                }while(arr[j] > pivot);
+                    if(i >= j)
+                        return j;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 }
