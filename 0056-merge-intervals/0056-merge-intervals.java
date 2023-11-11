5739cb10958b2cf6f5b0bc617d8229bc9d2426be
MD-MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-11-11 10:59:03 +0530

    Time: 9 ms (57.33%), Space: 45.2 MB (81.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         
-          if (intervals.length <= 1) return intervals;
- 
-        // Sort the intervals based on the start time
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
- 
+        if(intervals.length <= 1)
+            return intervals;
+        
+        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
         List<int[]> result = new ArrayList<>();
+        
         int[] currentInterval = intervals[0];
- 
-        for (int i = 1; i < intervals.length; i++) {
+        
+        for(int i=1;i<intervals.length;i++)
+        {
             int[] nextInterval = intervals[i];
- 
-            if (currentInterval[1] >= nextInterval[0]) {
-                // There is an overlap, merge the intervals
+            
+            if(currentInterval[1] >= nextInterval[0]){
+                //here there is an overlap so merge the intervals.
                 currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
-            } else {
-                // No overlap, add the current interval to the result and update currentInterval
+            }
+            
+            else{
+                //no overlap, add the current interval to the result and update currentInterval.
                 result.add(currentInterval);
                 currentInterval = nextInterval;
             }
         }
- 
-        // Add the last interval
+        //Adding the last interval
         result.add(currentInterval);
- 
+        
         return result.toArray(new int[result.size()][]);
     }
 }
-
