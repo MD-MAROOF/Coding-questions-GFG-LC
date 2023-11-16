c33774065fcb14603a15e60df1a9df2e6cffe1c1
MD-MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-11-16 10:43:45 +0530

    Added solution - LeetHub

---
+++ K-th element of two Arrays - GFG/kth-element-of-two-arrays.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+        int[] temp = new int[n+m];
+        
+        int i=0,j=0,ind=0;
+        
+        while(i < n && j < m)
+        {
+            if(arr1[i] <= arr2[j])
+            {
+                temp[ind] = arr1[i];
+                i++;ind++;
+            }
+            
+            else{
+                temp[ind] = arr2[j];
+            ind++;j++;
+            } 
+        }
+        
+        while(i < n)
+        {
+            temp[ind] = arr1[i];
+            i++;
+            ind++;
+        }
+        
+        while(j < m)
+        {
+            temp[ind] = arr2[j];
+            j++;
+            ind++;
+        }
+        
+       // System.out.println(Arrays.toString(temp));
+        
+    //    int ans = Integer.MIN_VALUE;
+      
+   /*   for(int z=1;z<=temp.length;z++)
+      {
+         if(z == k)
+         {
+             ans = temp[z];
+             break;
+         }
+      }*/
+        
+        return temp[k-1];
+        
+    }
+}
