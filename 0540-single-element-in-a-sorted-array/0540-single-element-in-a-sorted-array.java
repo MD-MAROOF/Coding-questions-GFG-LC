63eaed4a04193d8cafe13a499f3e4db9a4509e20
MD-MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-11-25 11:44:06 +0530

    Time: 0 ms (100.00%), Space: 49.2 MB (86.20%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,22 +1,33 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        int n = nums.length;
         
-        for(int i=0;i<nums.length;i++)
+        if(n == 1)
+            return nums[0];
+        
+        if(nums[0] != nums[1])
+            return nums[0];
+        
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+        
+        int low=1,high=n-2;
+        
+        while(low <= high)
         {
-            if(hm.containsKey(nums[i]))
-                hm.put(nums[i], hm.get(nums[i])+1);
+            int mid = (low + high)/2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+                return nums[mid];
+            
+            if((mid%2 == 1 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1]))
+            {
+                low = mid + 1;
+            }
+            
             else
-                hm.put(nums[i],1);
+                high = mid-1;
         }
-        
-        int ans = -1;
-        for(Map.Entry<Integer,Integer> entry : hm.entrySet())
-        {
-            if(entry.getValue() == 1)
-                ans = entry.getKey();
-        }
-        return ans;
+        return -1;
     }
 }
