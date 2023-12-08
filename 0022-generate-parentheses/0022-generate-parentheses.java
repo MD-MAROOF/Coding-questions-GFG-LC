555217bbee984199d277a8033f90840238b7865f
MD-MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-08 15:19:09 +0530

    Time: 2 ms (22.68%), Space: 43.4 MB (51.01%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+     ArrayList<String> ans = new ArrayList<>();
+        function(ans,n,0,0,"");
+        return ans;
+    }
+    
+     void function(ArrayList<String> ans,int n, int open, int close, String str)
+    {
+        if(str.length() == n*2)
+        {
+            ans.add(str);
+            return;
+        }
+        
+        if(open < n)
+        {
+            function(ans,n,open+1,close,str+"(");
+        }
+        
+        if(close < open)
+            function(ans,n,open,close+1,str+")");
+    }
+}
