21f4aba2b467507089595cfe96644cf480580382
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-20 19:56:26 +0530

    Time: 0 ms (100.00%), Space: 42 MB (5.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0,high = nums.length-1;
+        int low = 0, high = nums.length-1;
+        
         mergeSort(nums,low,high);
         
     }
     
-    void mergeSort(int[] nums,int low,int high)
+    void mergeSort(int[] nums, int low, int high)
     {
         if(high > low)
         {
-            int mid = low+(high-low)/2;
+            int mid = (low + high)/2;
             mergeSort(nums,low,mid);
             mergeSort(nums,mid+1,high);
             merge(nums,low,mid,high);
         }
     }
     
-    void merge(int[] arr, int low,int mid, int high)
+    void merge(int[] arr, int low, int mid, int high)
     {
         int n1 = mid-low+1;
         int n2 = high-mid;
@@ -25,30 +26,25 @@ class Solution {
         int[] right = new int[n2];
         
         for(int i=0;i<n1;i++)
-        {
-            left[i] = arr[low + i];
-        }
+            left[i] = arr[low+i];
         
         for(int i=0;i<n2;i++)
-        {
             right[i] = arr[mid+i+1];
-        }
         
         int i=0,j=0,k=low;
         
         while(i < n1 && j < n2)
         {
-            if(left[i] <= right[j])
-            {
-                arr[k] = left[i];
-                i++;
-                k++;
-            }
+           if(left[i] <= right[j])
+           {
+               arr[k] = left[i];
+               i++;
+               k++;
+           }
             
             else{
                 arr[k] = right[j];
-                k++;
-                j++;
+                k++;j++;
             }
         }
         
@@ -62,7 +58,8 @@ class Solution {
         while(j < n2)
         {
             arr[k] = right[j];
-            j++;k++;
+            j++;
+            k++;
         }
     }
 }
