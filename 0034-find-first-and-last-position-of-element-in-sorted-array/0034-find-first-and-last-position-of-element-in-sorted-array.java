757736ddae3d76bd96764b83ec2b1423b1130956
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-20 19:46:31 +0530

    Time: 0 ms (100.00%), Space: 46.3 MB (8.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        
         int start_index = -1;
-        int start = 0;
-        int end = nums.length-1;      //firstly I am trying to find 
-                                      //       start index
+        int start = 0, end = nums.length-1;
         
         while(start <= end)
         {
-            int mid = start + (end-start)/2;
+            int mid = (start + end)/2;
             if(nums[mid] == target)
             {
                 start_index = mid;
@@ -23,28 +22,26 @@ class Solution {
         
         start = 0;
         end = nums.length-1;
-        int end_index = -1;                //now I an trying to find 
-                                           //      end index
-        
+        int end_index = -1;
         
         while(start <= end)
         {
-            int mid = start + (end-start)/2;
-            
+            int mid = (start + end)/2;
             if(nums[mid] == target)
             {
                 end_index = mid;
-                start = mid+1;
+                start = mid + 1;
             }
             
             else if(target > nums[mid])
                 start = mid + 1;
             
             else if(target < nums[mid])
-                end = mid-1;
+                end = mid - 1;
         }
         
         int[] ans = new int[2];
+        
         ans[0] = start_index;
         ans[1] = end_index;
         
