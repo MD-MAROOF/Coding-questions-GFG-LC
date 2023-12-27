70c75102cc75b77e1d24d621a682e133177a63fe
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-27 14:49:27 +0530

    Time: 4 ms (66.09%), Space: 42.6 MB (72.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        LinkedHashMap<Character, Integer> map1 = new LinkedHashMap<>();
-        LinkedHashMap<Character, Integer> map2 = new LinkedHashMap<>();
+      
+        
+        if(s.length() != t.length())
+            return false;
+        
+      int[] count = new int[256];
         
         for(int i=0;i<s.length();i++)
         {
-            if(map1.containsKey(s.charAt(i)))
-                map1.put(s.charAt(i),map1.get(s.charAt(i))+1);
-            else
-                map1.put(s.charAt(i),1);
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
         }
         
-         for(int i=0;i<t.length();i++)
+        for(int i=0;i<count.length;i++)
         {
-            if(map2.containsKey(t.charAt(i)))
-                map2.put(t.charAt(i),map2.get(t.charAt(i))+1);
-            else
-                map2.put(t.charAt(i),1);
+            if(count[i] != 0)
+                return false;
         }
         
-        return map1.equals(map2);
+        return true;
     }
 }
