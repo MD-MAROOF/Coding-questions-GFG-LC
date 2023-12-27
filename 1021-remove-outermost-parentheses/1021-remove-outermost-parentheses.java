bf563249ae37cbc827a49c59f022463eaf793c5c
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-27 12:22:57 +0530

    Time: 18 ms (5.61%), Space: 44.8 MB (6.42%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        
+        int count = 0;
+        boolean flag=true;
+        String ans = "";
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == '(')
+                count++;
+            
+            else 
+                count--;
+            
+            if(count == 1 && flag == true){
+                flag = false;
+                continue;
+            }
+            
+            if(count==0 && flag == false)
+            {
+                flag = true;
+                continue;
+            }
+            
+            ans = ans + s.charAt(i);
+        }
+        return ans;
+    }
+}
