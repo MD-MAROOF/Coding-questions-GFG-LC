b0ca57566d26008881a3690851f6b91e1eb788ae
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-28 20:53:40 +0530

    Time: 36 ms (49.20%), Space: 42.1 MB (68.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,57 +1,66 @@
 class Solution {
     public String longestPalindrome(String s) {
-      if(s.length() <= 1)
-          return s;
+        if(s.length() <= 1)
+            return s;
         
         int max = 1;
-        int n=s.length();
-        int st=0,end=0;
+        int n = s.length();
+        int st = 0,end=0;
+        
+        //this code is for odd length substring
         
         for(int i=0;i<n-1;i++)
         {
-            int l=i,r=i;
+            int l = i,r=i;
+            
             while(l >= 0 && r < n)
             {
-                if(s.charAt(l) == s.charAt(r)){
-                l--;
-                r++;
+                if(s.charAt(l) == s.charAt(r))
+                {
+                    l--;
+                    r++;
                 }
-            
-            else
-                break;
-        }
-        
-        int len=r-l-1;
-        if(len > max)
-        {
-            max = len;
-            st = l+1;
-            end = r-1;
-        }
-    }
-    
-    for(int i=0;i<n-1;i++)
-    {
-        int l=i,r=i+1;
-        while(l >= 0 && r < n)
-        {
-            if(s.charAt(l) == s.charAt(r))
-            {
-                l--;
-                r++;
+                
+                else
+                    break;
             }
-            else
-                break;
+            
+            int len = r-l-1;
+            if(len > max)
+            {
+                max = len;
+                st = l+1; 
+                end = r-1;
+            }
         }
         
-        int len = r-l-1;
-        if(len > max)
+        //this code is for even length substring
+        
+        for(int i=0;i<n-1;i++)
         {
-            max = len;
-            st = l+1;
-            end = r-1;
+            int l = i, r = i+1;
+            
+            while(l >=0 && r < n)
+            {
+                if(s.charAt(l) == s.charAt(r))
+                {
+                    l--;
+                    r++;
+                }
+                
+                else
+                    break;
+            }
+            
+            int len = r-l-1;
+            if(len > max)
+            {
+                max = len;
+                st = l+1;
+                end = r-1;
+            }
         }
-    }
+        
         return s.substring(st,end+1);
-}
+    }
 }
