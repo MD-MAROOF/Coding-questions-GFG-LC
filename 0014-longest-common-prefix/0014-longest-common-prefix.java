0222bd8996a78dfe359eb879eea4e713a7b7a378
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2023-12-29 11:19:34 +0530

    Time: 4 ms (20.33%), Space: 41.3 MB (23.58%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+ 
+     /*   The big brained concept here seems to be this:
+
+If the array is sorted alphabetically then you can assume that the first element of the array and the last element of the array will have most different prefixes of all comparisons that could be made between strings in the array. If this is true, you only have to compare these two strings */
+        
+        //Understand the above commented lines carefully to understand the solution
+        
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
+        }
+        return ans.toString();
+    }
+}
