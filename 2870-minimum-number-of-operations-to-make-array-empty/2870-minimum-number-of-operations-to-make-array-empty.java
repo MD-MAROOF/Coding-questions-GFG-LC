35f2bfb60a9e36e013dc8931fa621e368fa771b6
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-01-04 15:03:49 +0530

    Time: 29 ms (21.89%), Space: 56.9 MB (53.21%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,31 +1,42 @@
 class Solution {
     public int minOperations(int[] nums) {
         
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        /* 
+1. It uses a map mp to calculate the frequency of each number in nums.
+
+2. If any number occurs only once in nums, it's impossible to achieve the desired pattern, so the function returns -1.
+
+3.It calculates the number of moves needed to make all occurrences of each number a multiple of 3. If there are remaining occurrences (not divisible by 3), it adds one more move for each such occurrence.
+
+4. Finally, it returns the total number of moves needed to achieve the desired pattern.
+        */
+        
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
         
         for(int i=0;i<nums.length;i++)
         {
-            if(hm.containsKey(nums[i]))
-                hm.put(nums[i],hm.get(nums[i])+1);
+            if(map.containsKey(nums[i]))
+                map.put(nums[i],map.get(nums[i])+1);
             else
-                hm.put(nums[i],1);
+                map.put(nums[i],1);
+            
         }
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        int count = 0;
         
-        for(Map.Entry<Integer,Integer> entry : hm.entrySet())
+        int count = 0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet())
         {
-          int t = entry.getValue();
+            int t = entry.getValue();
+            
             if(t == 1)
                 return -1;
             
-              count += t / 3; // Count the complete sets of 3 elements
-            if (t % 3 != 0) // If there are remaining elements, increment the count
+            count = count + (t/3);
+            if(t % 3 != 0)
                 count++;
         }
         
         return count;
-        
     }
 }
