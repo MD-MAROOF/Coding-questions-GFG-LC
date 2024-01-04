e3996e13eba9143c4ae661921b87a767ead94eb6
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-01-04 19:17:29 +0530

    Time: 24 ms (36.60%), Space: 57 MB (49.43%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -1,16 +1,6 @@
 class Solution {
     public int minOperations(int[] nums) {
-        
-        /* 
-1. It uses a map mp to calculate the frequency of each number in nums.
 
-2. If any number occurs only once in nums, it's impossible to achieve the desired pattern, so the function returns -1.
-
-3.It calculates the number of moves needed to make all occurrences of each number a multiple of 3. If there are remaining occurrences (not divisible by 3), it adds one more move for each such occurrence.
-
-4. Finally, it returns the total number of moves needed to achieve the desired pattern.
-        */
-        
         
         HashMap<Integer,Integer> map = new HashMap<>();
         
@@ -22,7 +12,6 @@ class Solution {
                 map.put(nums[i],1);
             
         }
-        
         
         int count = 0;
         for(Map.Entry<Integer,Integer> entry : map.entrySet())
