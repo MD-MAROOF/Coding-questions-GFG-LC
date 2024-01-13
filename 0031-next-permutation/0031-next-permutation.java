94115adf1fb29a5cbcf04145442287fefc88c1f9
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-01-13 11:03:03 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (51.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         
-       if(nums.length == 1)
-           return;
+        
+        if(nums.length == 1)
+            return;
         
         int idx1 = Integer.MIN_VALUE;
+        
         for(int i=nums.length-2;i>=0;i--)
         {
-            if((nums[i] < nums[i+1]))
-            {
+            if(nums[i] < nums[i+1]){
                 idx1 = i;
                 break;
             }
         }
         
         if(idx1 < 0)
+        {
             reverse(nums,0,nums.length-1);
+        }
+        
         else{
-        int idx2=0;
-       for(int i=nums.length-1;i>=0;i--)
-       {
-           
-           if(nums[i] > nums[idx1])
-           {
-               idx2 = i;
-               break;
-           }
-       }
-        
-       int temp = nums[idx1];
-        nums[idx1] = nums[idx2];
-        nums[idx2] = temp;
-        
-        reverse(nums,idx1+1,nums.length-1);
+            int idx2 = 0;
+            for(int i=nums.length-1;i>=0;i--)
+            {
+                if(nums[i] > nums[idx1]){
+                    idx2 = i;
+                    break;
+                }
+            }
+            
+            int temp = nums[idx1];
+            nums[idx1] = nums[idx2];
+            nums[idx2] = temp;
+            
+            reverse(nums,idx1+1,nums.length-1);
         }
     }
     
-    void reverse(int[] nums,int start,int end)
+    void reverse(int[] nums, int start, int end)
     {
         while(start <= end)
         {
