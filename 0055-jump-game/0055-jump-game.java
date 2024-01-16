f782d226f50de0446c7a636752e839eed46a924c
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-01-16 12:46:01 +0530

    Time: 1 ms (99.98%), Space: 45.6 MB (24.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-      int index = nums.length-1;
         
-        for(int i=nums.length-1;i>=0;i--)
+        int n = nums.length;
+        
+        int index = n-1;
+        for(int i=n-1;i>=0;i--)
         {
-            if(nums[i] + i >= index)
+            if(i + nums[i] >= index)
                 index = i;
         }
         
@@ -13,7 +15,6 @@ class Solution {
             return true;
         
         return false;
-            
-        
     }
+    
 }
