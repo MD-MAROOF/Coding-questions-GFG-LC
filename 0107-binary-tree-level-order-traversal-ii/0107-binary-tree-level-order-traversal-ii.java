89f235552491c3958b72c1d9d721d6459e394322
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-02-07 16:00:13 +0530

    Time: 1 ms (93.13%), Space: 42.8 MB (45.79%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -15,21 +15,33 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> res = new ArrayList();
-        if(root == null) return res;
-        Queue<TreeNode> q = new LinkedList();
-        q.add(root);
-        while(!q.isEmpty()){
-            int len = q.size();
-            List<Integer> temp = new ArrayList();
-            for(int i=0; i<len; i++){
-                TreeNode cur = q.poll();
-                temp.add(cur.val);
-                if(cur.left != null) q.add(cur.left);
-                if(cur.right != null) q.add(cur.right);
+           Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        if(root == null)
+            return ans;
+        
+        queue.offer(root);
+        
+        while(!queue.isEmpty())
+        {
+            int n = queue.size();
+            List<Integer> subList = new ArrayList<Integer>();
+            
+            for(int i=0;i<n;i++)
+            {
+                if(queue.peek().left != null)
+                    queue.offer(queue.peek().left);
+                
+                if(queue.peek().right != null)
+                    queue.offer(queue.peek().right);
+                
+                subList.add(queue.poll().val);
+                
             }
-            res.add(0, temp);
+            ans.add(0,subList);
         }
-        return res;
+        
+        return ans;
     }
 }
