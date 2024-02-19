4fc436cf66cb980a17f69cd5c883262fa5285e05
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-02-19 18:46:17 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (16.55%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,7 +17,6 @@ class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
         dfs(root,0);
         return ans;
     }
@@ -27,9 +26,11 @@ class Solution {
         if(root == null)
             return;
         
-        
         if(ans.size() == level)
+        {
             ans.add(new ArrayList<>());
+        }
+        
         ans.get(level).add(root.val);
         
         dfs(root.left,level+1);
