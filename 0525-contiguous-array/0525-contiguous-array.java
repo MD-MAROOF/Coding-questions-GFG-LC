13047a2b880e42548305cc1347b1a8c5954e0251
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-03-16 19:43:22 +0530

    Time: 24 ms (57.31%), Space: 55.1 MB (55.25%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findMaxLength(int[] nums) {
+        //To find the maximum length of a contiguous subarray with an equal number of 0s and 1s, we can use the concept of prefix sum. Whenever we encounter a 0, we decrement the sum by 1, and whenever we encounter a 1, we increment the sum by 1. If the prefix sum at two indices is the same, it means that the number of 0s and 1s between those two indices is the same. We store these prefix sums along with their indices in a hash map. Then, for each prefix sum encountered, we check if we have seen this sum before. If so, it means that the subarray between the current index and the index where this sum was last encountered has an equal number of 0s and 1s. We calculate the length of this subarray and update the maximum length accordingly.
+        
         
         int n = nums.length;
         HashMap<Integer,Integer> map = new HashMap<>();
