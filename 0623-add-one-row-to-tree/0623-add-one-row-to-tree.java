c426d421f4adabe682bb1d2f1e853227fbac7ca9
MD MAROOF <98028071+MD-MAROOF@users.noreply.github.com>
2024-04-14 19:20:56 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (64.07%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        
+        if(depth == 1)
+        {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        depth--;
+        queue.offer(root);
+        int level = 1;
+        
+        while(level != depth)
+        {
+            int n = queue.size();
+            level++;
+            
+            for(int i=0;i<n;i++)
+            {
+                TreeNode temp = queue.poll();
+                if(temp.left != null)
+                    queue.offer(temp.left);
+                
+                if(temp.right != null)
+                    queue.offer(temp.right);
+            }
+        }
+        
+       
+        while(!queue.isEmpty())
+        {
+            TreeNode temp = queue.poll();
+            TreeNode leftChild = temp.left;
+            TreeNode rightChild = temp.right;
+            
+            TreeNode newLeft = new TreeNode(val);
+            TreeNode newRight = new TreeNode(val);
+            
+            temp.left = newLeft;
+            temp.right = newRight;
+            
+            newLeft.left = leftChild;
+            newRight.right = rightChild;
+        }
+        
+        return root;
+    }
+}
